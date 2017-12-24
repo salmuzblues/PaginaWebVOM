d2ba790beaa7fe706a67e1990ac12649fbcf146d
juan.carranza <salmuzblues@gmail.com>
2017-12-24 17:37:16 -0500

    adding field.jpg

--- src/assets/switchColor.js
+++ src/assets/switchColor.js
@@ -7,7 +7,7 @@ app.directive("scroll" ,function ($window) {
   return function($scope){
     angular.element($window).bind("scroll", function () {
 
-      if (this.pageYOffset >= 150) {
+      if (this.pageYOffset >= 50) {
         $scope.boolChangeClass = true;
       } else {
         $scope.boolChangeClass = false;
